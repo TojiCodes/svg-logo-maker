13fd5b4b0f1e3312ec31508d7953588396df37dd
/\///\/_//\ _ <tojicodes@gmail.com>
2023-07-03 01:21:43 -0500

    Everything should work correctly now

--- Develop/lib/svg.js
+++ Develop/lib/svg.js
@@ -1,7 +1,7 @@
 class SVG {
 static generateLogo(color, shape, text) {
     const shapeMarkup = shape.render();
-    const textMarkup = `<text x="50%" y="50%" fill="white" dominant-baseline="middle" text-anchor="middle" font-size="24">${text}</text>`;
+    const textMarkup = `<text x="50%" y="50%" fill="white" dominant-baseline="middle" text-anchor="middle" font-size="24" alignment-baseline="middle">${text}</text>`;
 
     const svgContent = `
         <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
