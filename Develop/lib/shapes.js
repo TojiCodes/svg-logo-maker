5c4e615288d995a39c62853fac51f944d1f274d4
/\///\/_//\ _ <tojicodes@gmail.com>
2023-07-03 01:11:36 -0500

    Centered Shapes

--- Develop/lib/shapes.js
+++ Develop/lib/shapes.js
@@ -1,29 +1,29 @@
 // Base class for shapes
 class Shape {
 constructor() {
-        this.color = "";
+    this.color = "";
 }
 setColor(color) {
-        this.color = color;
-    }
+    this.color = color;
+}
 }
 
 class Circle extends Shape {
-    render() {
-        return `<circle cx="150" cy="100" r="80" fill="${this.color}" />`;
-    }
+render() {
+    return `<circle cx="100" cy="100" r="80" fill="${this.color}" />`;
+}
 }
 
 class Triangle extends Shape {
-    render() {
-        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
-    }
+render() {
+    return `<polygon points="100, 18 194, 182 6, 182" fill="${this.color}" />`;
+}
 }
 
 class Square extends Shape {
-    render() {
-        return `<rect x="90" y="40" width="120" height="120" fill="${this.color}" />`;
-    }
+render() {
+    return `<rect x="40" y="40" width="120" height="120" fill="${this.color}" />`;
+}
 }
 
 module.exports = { Circle, Triangle, Square };
