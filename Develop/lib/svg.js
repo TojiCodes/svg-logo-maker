707fc97cbb3d0bb75afe569c147b750a12b09711
/\///\/_//\ _ <tojicodes@gmail.com>
2023-07-03 00:47:09 -0500

    trying to get command line to work properly

--- Develop/lib/svg.js
+++ Develop/lib/svg.js
@@ -1,11 +1,13 @@
 class SVG {
 static generateLogo(color, shape, text) {
-    const shapeMarkup = shape.render(); // Call the render method of the shape object
+    const shapeMarkup = shape.render();
     const textMarkup = `<text x="50%" y="50%" fill="white" dominant-baseline="middle" text-anchor="middle" font-size="24">${text}</text>`;
 
     const svgContent = `
         <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
-        ${shapeMarkup}
+        <g transform="translate(100, 100)">
+            ${shapeMarkup}
+        </g>
         ${textMarkup}
         </svg>
     `;
@@ -13,5 +15,3 @@ static generateLogo(color, shape, text) {
     return svgContent;
 }
 }
-
-module.exports = SVG;
